22e225b161f3df8c2822e447e841b16a9c34078d
Nooma <tom_bradley123@hotmail.co.uk>
2019-02-11 14:19:36 +0000

    init electron app

---
+++ main.js
@@ -0,0 +1,12 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({ width: 800, height: 600 });
+	win.setMenuBarVisibility(false)
+  // and load the index.html of the app.
+  win.loadFile('index.html');
+  win.setResizable(false);
+}
+
+app.on('ready', createWindow);
