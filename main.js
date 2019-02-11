3561d41a18752eabef0e7a85e0924541b9c2bb6c
Nooma <tom_bradley123@hotmail.co.uk>
2019-02-11 16:07:05 +0000

    begin creating login page

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const { app, BrowserWindow } = require('electron');
+const {ipcMain} = require('electron')
+const { net } = require('electron')
 
 function createWindow () {
   // Create the browser window.
   let win = new BrowserWindow({ width: 800, height: 600 });
-	win.setMenuBarVisibility(false)
+  //win.openDevTools();
+  win.setMenuBarVisibility(false)
   // and load the index.html of the app.
   win.loadFile('index.html');
   win.setResizable(false);
+}
+
+
+function login() {
+	console.log("click!");
 }
 
 app.on('ready', createWindow);
