40f62eb8d26776b31c96834e871f3e4e093e8719
Nooma <tom_bradley123@hotmail.co.uk>
2019-02-15 16:31:31 +0000

    continue designing home page tabs

--- homeRenderer.js
+++ homeRenderer.js
@@ -1,3 +1,6 @@
+var QRCode = require('qrcode');
+
+
 socket = io.connect('http://localhost:9001', {
 		'connect timeout': 5000,
 		'reconnectionAttempts': 3
@@ -12,7 +15,9 @@ socket.on('connect', function () {
 });
 
 
-function changeTab(evt, cityName) {
+function changeTab(evt, tabName) {
+
+	
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -22,6 +27,21 @@ function changeTab(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  
+    if (tabName == "JoinCode")
+  {
+	var qrCanvas = document.getElementById('qrCanvas');
+
+	
+	QRCode.toCanvas(qrCanvas, 'sample text',{ width: 200 }, function (error) {
+	  if (error) console.error(error)
+	  console.log('success!');
+	})	  
+  }
+  document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
+}
+
+function roomSelect(clickedID) {
+	console.log("click room " + clickedID + "!");
 }
