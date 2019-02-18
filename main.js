380e82debbb755b9f526929fd9ad03f1412c28b6
Nooma <tom_bradley123@hotmail.co.uk>
2019-02-18 15:26:14 +0000

    dev tool new window, continue home UI dev

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ const { net } = require('electron')
 
 function createWindow () {
   // Create the browser window.
-  let win = new BrowserWindow({ width: 800, height: 600 });
-  win.openDevTools();
+  let win = new BrowserWindow({  minHeight: 600,
+  minWidth: 800, nodeIntegration: true, width: 800, height: 600 });
+  
+  let devtools = new BrowserWindow();
+   win.webContents.setDevToolsWebContents(devtools.webContents)
+  win.webContents.openDevTools({mode: 'detach'});
+  
   win.setMenuBarVisibility(false)
   // and load the index.html of the app.
   win.loadFile('index.html');
-  win.setResizable(false);
+  //win.setResizable(false);
 }
 
 app.on('ready', createWindow);
